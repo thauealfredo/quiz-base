1fad29cbbc3d1b19edaf4e006cb097d9c930977f
Thaue <thaue.alfredo@gmail.com>
2021-01-25 14:41:55 -0300

    change git url

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ export default function Home() {
         </Widget>
         <Footer />
       </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/omariosouto" />
+      <GitHubCorner projectUrl="https://github.com/thauealfredo" />
     </QuizBackground>
   );
 }
